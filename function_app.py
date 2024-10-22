53fd1ad8e81a07bd54d799fea86b0f92b6337cf5
RoSosaTec <a01027913@tec.mx>
2024-10-22 11:19:37 -0600

    nuevos códigos de generación de imágenes y prompts y reporte del día 2

--- function_app.py
+++ function_app.py
@@ -1,6 +1,10 @@
 import azure.functions as func
 import logging
+from openai import OpenAI
 
+secret_key = "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"
+
+# {"model": "gpt-3.5-turbo", "prompt": "Write a romantic story about an alien and a human astronaut", "max_tokens": 100, "temperature": 1}
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="firstfunctionapi")
@@ -23,3 +27,48 @@ def firstfunctionapi(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.route(route="completionAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def completionAPI(req: func.HttpRequest) -> func.HttpResponse:
+
+    logging.info('Python HTTP trigger function processed a request.')
+
+    client = OpenAI(
+    api_key = secret_key
+    )
+    
+    req_body = req.get_json()
+
+    completion = client.chat.completions.create(
+    model = req_body["model"],
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": req_body["prompt"]}
+    ],
+    max_tokens = req_body["max_tokens"],
+    temperature = req_body["temperature"],
+    )
+
+    return func.HttpResponse(
+        completion.choices[0].message.content, status_code = 200
+        )
+
+@app.route(route="imageGenAPI", auth_level=func.AuthLevel.ANONYMOUS)
+def imageGenAPI(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+        
+    client = OpenAI(
+        api_key = secret_key
+    )
+
+    req_body = req.get_json()
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt=req_body["prompt"],
+        size="1024x1024",
+        quality="standard",
+        n=1,
+        )
+    
+    return ( response.data[0].url )
